0b8ef905d43c8b6a2978e928dcb707cc3ed6b9d6
Josh Arnold <josharnold@Joshs-MacBook-Pro.local>
2020-12-26 17:21:56 -0800

    intial commit

---
+++ MessageBubbleView/MessageBubbleView.h
@@ -0,0 +1,18 @@
+//
+//  MessageBubbleView.h
+//  MessageBubbleView
+//
+//  Created by Josh Arnold on 12/26/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MessageBubbleView.
+FOUNDATION_EXPORT double MessageBubbleViewVersionNumber;
+
+//! Project version string for MessageBubbleView.
+FOUNDATION_EXPORT const unsigned char MessageBubbleViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessageBubbleView/PublicHeader.h>
+
+
