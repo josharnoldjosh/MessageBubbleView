86cc099070cee7935b40fb8bfc52b9851b6c8de5
Josh Arnold <josharnold@Joshs-MacBook-Pro.local>
2020-12-27 10:42:29 -0800

    push dummy code

---
+++ MessageBubbleView/MessageBubbleView.swift
@@ -0,0 +1,23 @@
+//
+//  MessageBubbleView.swift
+//  MessageBubbleView
+//
+//  Created by Josh Arnold on 12/26/20.
+//
+
+import UIKit
+
+
+public final class MessageBubbleView {
+
+    let name = "MessageBubbleView"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- MessageBubbleViewTests/MessageBubbleViewTests.swift
+++ MessageBubbleViewTests/MessageBubbleViewTests.swift
@@ -8,26 +8,21 @@
 import XCTest
 @testable import MessageBubbleView
 
+
 class MessageBubbleViewTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var bubbleView:MessageBubbleView!
+    
+    override func setUp() {
+        bubbleView = MessageBubbleView()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAdd() {
+        XCTAssertEqual(bubbleView.add(a: 1, b: 1), 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSub() {
+        XCTAssertEqual(bubbleView.sub(a: 1, b: 1), 0)
+        XCTAssertEqual(bubbleView.sub(a: 20, b: 6), 14)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
